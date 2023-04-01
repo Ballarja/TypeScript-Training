5780155e00361af29a16ca9a5b64200ab55ff120
Ballarja <JamesBallard87@hotmail.com>
2023-04-01 00:10:06 -0500

    used push to added string variables to array then used for of loop to loop through with the console.log to print them

---
+++ 03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,7 @@
+var sportsTwo = ["Golf", "Baseball", "Tennis"];
+sportsTwo.push("Football");
+sportsTwo.push("Soccer");
+for (var _i = 0, sportsTwo_1 = sportsTwo; _i < sportsTwo_1.length; _i++) {
+    var tempSport = sportsTwo_1[_i];
+    console.log(tempSport);
+}
---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf","Baseball","Tennis"];
+
+sportsTwo.push("Football");
+sportsTwo.push("Soccer");
+
+for(let tempSport of sportsTwo){
+    console.log(tempSport);
+}
