203c240be19c3b6beb9b86699f44ffb5ada48d09
Ballarja <JamesBallard87@hotmail.com>
2023-03-31 23:59:24 -0500

    created new file for somemore loop practice

---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,14 @@
+let sportsOne: string[] = ["Golf","Baseball", "Tennis","Soccer"];
+
+// for(let i = 0; i < sportsOne.length; i++){
+//     console.log(sportsOne[i]);
+// }
+
+for(let tempSport of sportsOne){
+    if(tempSport === "Baseball"){
+        console.log(`${tempSport} is my favorite sport`)
+    }
+    else{
+
+    }
+}
