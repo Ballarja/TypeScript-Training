d4c24636874fcb9b1e21dcac21957386a87574cf
Ballarja <JamesBallard87@hotmail.com>
2023-03-31 23:14:45 -0500

    created 4 variables in sample-types.ts then complied it to a js file and logged 3 messages

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Jim";
+var lastName = "Ballard";
+console.log(found);
+console.log("The grade is ".concat(grade));
+console.log("Hi ".concat(firstName, " ").concat(lastName));
